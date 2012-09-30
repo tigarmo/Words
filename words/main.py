e8502482c87a337b9f1d4e113b23bd70017c9784
Tiago Nobrega <tigarmo@gmail.com>
2012-09-30 14:23:12 -0300

    allWords() returns a list of all valid words.
    Updated Wordtree.allWords() to return a list of all valid words
    for a given sequence of letters. Includes words of all sizes in
    the inclusive range [3,len(letters)].

--- words/main.py
+++ words/main.py
@@ -1,23 +1,22 @@
 from tree import WordTree
 
+
 def main():
-	with open("/usr/share/dict/words", 'r') as f:
-		l = []
-		for line in f:
-			l.append(line.strip())
+    with open("/usr/share/dict/words", 'r') as f:
+        l = []
+        for line in f:
+            l.append(line.strip())
 
-	wt = WordTree()
-	for line in l:
-		wt.addWord(line)
+    wt = WordTree()
+    for line in l:
+        wt.addWord(line)
 
-	while True:
-		print "Enter a word:"
-		s = raw_input().strip()
-		if wt.isWord(s):
-			print s + " IS a word."
-		else:
-			print s + " IS NOT a word."
+    while True:
+        print "Enter a letter combination:"
+        s = raw_input().strip()
+        words = wt.allWords(s)
+        for w in words:
+            print "> " + w
 
 if __name__ == "__main__":
-	main()
-
+    main()
--- words/tree.py
+++ words/tree.py
@@ -1,53 +1,83 @@
+import itertools
+
+
 class Node:
-	def __init__(self, char = ''):
-		self.char = char
-		self.children = {}
-		self.final = False
-	
-	def addWord(self,word):
-		if not word:
-			self.final = True
-			return
+    def __init__(self, char=''):
+        self.char = char
+        self.children = {}
+        self.final = False
 
-		c = word[0]
-		n = self.children.get(c)
-		if not n:
-			n = Node(c)
-			self.children[c] = n
+    def addWord(self, word):
+        if not word:
+            self.final = True
+            return
 
-		n.addWord(word[1:])
+        c = word[0]
+        n = self.children.get(c)
+        if not n:
+            n = Node(c)
+            self.children[c] = n
 
-	def isWord(self,word):
-		if not word:
-			return self.final
+        n.addWord(word[1:])
 
-		c = word[0]
-		n = self.children.get(c)
-		if not n:
-			return False
+    def isWord(self, word):
+        if not word:
+            return self.final
 
-		return n.isWord(word[1:])
+        c = word[0]
+        n = self.children.get(c)
+        if not n:
+            return False
+
+        return n.isWord(word[1:])
 
 
 class WordTree:
-	def __init__(self):
-		self.head = Node('')
+    def __init__(self):
+        self.head = Node('')
 
-	def addWord(self, word):
-		self.head.addWord(word)
+    def addWord(self, word):
+        self.head.addWord(word)
 
-	def prettyprint(self):
-		s = ""
-		n = self.head
-		while n:
-			s += "-- " + n.char
-			if n.children.values():
-				n = n.children.values()[0]
-			else:
-				n = None
+    def prettyprint(self):
+        s = ""
+        n = self.head
+        while n:
+            s += "-- " + n.char
+            if n.children.values():
+                n = n.children.values()[0]
+            else:
+                n = None
 
-		print s
+        print s
 
-	
-	def isWord(self,word):
-		return self.head.isWord(word)
+    def isWord(self, word):
+        return self.head.isWord(word)
+
+    def allWords(self, letters):
+        all_valid_words = []
+        for l in xrange(3, len(letters) + 1):
+            all_words = itertools.permutations(letters, l)
+            all_words = list(unique_everseen(all_words))
+            valid_words = [word for word in all_words if self.isWord(word)]
+            for i in valid_words:
+                all_valid_words.append("".join(i))
+        return all_valid_words
+
+
+def unique_everseen(iterable, key=None):
+    "List unique elements, preserving order. Remember all elements ever seen."
+    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
+    # unique_everseen('ABBCcAD', str.lower) --> A B C D
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in itertools.ifilterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
